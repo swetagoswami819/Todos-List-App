3b125457cd41b506c76137df9e955303e889e157
sweta <sg8528224@gmail.com>
2025-07-25 15:14:09 +0530

    todos and todoitem updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import './App.css';
 import Header from "./MyComponents/Header"
 import {Todos} from "./MyComponents/Todos"
 import {Footer} from "./MyComponents/Footer"
+import React, {useState} from 'react'
 function App() {
+const onDelete = (todo) => {
+  console.log("I am onDelete function from App.js",todo);
+  //Deleting this way in react doess not work-->
+  //let index=todos.indexOf(todo);
+  //todos.splice(index,1);
 
-  let todos = [
+  setTodos(todos.filter((e)=>{
+    return e!==todo;
+
+  }));
+
+}
+  const [todos, setTodos] = useState([
     {
     sno : 1,
     title : "Go to the Market",
@@ -21,12 +33,13 @@ function App() {
       desc : "You need to go to the gym to exercise"
     }
   
-  ]
+  ]);
+
   return (
     <>
     <Header title="My Todos List" searchBar={true}/>
-    <Todos todos={todos}/>
-    <Footer/>
+    <Todos todos={todos} onDelete={onDelete}/>
+  
     </>
   );
 }
--- src/MyComponents/Todos.jsx
+++ src/MyComponents/Todos.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Todoitem} from './Todoitem';
 
-export const Todos = () => {
+export const Todos = (props) => {
   return (
-    <div>
-        Todos Works!
-      
+    <div className="container">
+        <h3 className='text-center'>Todos List</h3>
+        {props.todos.length==0?"No Todos to display" : 
+        props.todos.map((todo) => {
+            return <Todoitem todo={todo} onDelete={props.onDelete}/>
+        })}
     </div>
   )
 }
