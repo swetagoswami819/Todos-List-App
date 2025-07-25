0196124d213236bb4607af397786a397d5065359
sweta <sg8528224@gmail.com>
2025-07-25 14:21:19 +0530

    Header updated

--- src/App.js
+++ src/App.js
@@ -1,65 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from "./MyComponents/Header"
+import {Todos} from "./MyComponents/Todos"
+import {Footer} from "./MyComponents/Footer"
 function App() {
-  return (
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
 
+  let todos = [
+    {
+    sno : 1,
+    title : "Go to the Market",
+    desc : "You need to go to the market to buy vegetables"
+    },
+    {
+      sno  : 2,
+      title : "Go to the Mall",
+      desc : "You need to go to the mall to buy clothes"
+    },
+    {
+      sno  : 3,
+      title : "Go to the Gym",
+      desc : "You need to go to the gym to exercise"
+    }
+  
+  ]
+  return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    
-    
-    
+    <Header title="My Todos List" searchBar={true}/>
+    <Todos todos={todos}/>
+    <Footer/>
     </>
   );
 }
